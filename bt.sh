bb96052c558a3e3a0eab9a41864d43359e81cec9
RPOL <87131317+POLROTY@users.noreply.github.com>
2023-02-16 23:01:02 +0100

    updated bt.sh

--- bt.sh
+++ bt.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
-
-# Define the MAC address of the Bluetooth device
-device_mac="XX:XX:XX:XX:XX:XX"
-
-# Turn on Bluetooth
-echo -e "power on\n" | bluetoothctl
-
-# Wait for Bluetooth to turn on
-sleep 3
-
-# Scan for the Bluetooth device
-echo -e "scan on\n" | bluetoothctl
-
-# Wait for the scan to complete
-sleep 15
-
-# Connect to the Bluetooth device
-echo -e "connect $device_mac\nquit" | bluetoothctl
-
-# Wait for the connection to complete
+bluetoothctl scan on & pid=$!
 sleep 5
-
-# Verify that the device is connected
-connected_devices=$(hcitool con | grep "$device_mac")
-if [[ -z "$connected_devices" ]]; then
-  echo "Error: Failed to connect to the device."
-  exit 1
-else
-  echo "Successfully connected to the device."
-  exit 0
-fi
+kill $pid > /dev/null 2>&1
+bluetoothctl pair XX:XX:XX:XX:XX:XX 
+bluetoothctl trust XX:XX:XX:XX:XX:XX
+bluetoothctl connect XX:XX:XX:XX:XX:XX
